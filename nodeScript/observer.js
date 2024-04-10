36ec94525558a6b0e480fc714e10e583933226aa
Aiden <aiden@AidendeMacBook-Pro.local>
2024-04-10 19:47:04 +0800

    数据库

--- nodeScript/observer.js
+++ nodeScript/observer.js
@@ -1,9 +1,16 @@
+/**
+ * 本地node服务，定时轮询 score_log ，把uploaded为0的数据 http post到云函数
+ * 一次调用，批量上传多条数据
+ */
 const sqlite3 = require('sqlite3').verbose();
 const axios = require('axios');
+const CLOUD_FUNCTION_HTTPPATH = 'postfromlocal'
 // 定义一个轮询间隔
-const POLL_INTERVAL = 5000; // 每5000毫秒（5秒）检查一次
+const POLL_INTERVAL = 1000 * 60; // 每5000毫秒（5秒）检查一次
+const DB_PATH = '/Users/aiden/code/db/kart.db';
+// const DB_PATH = '/Users/joy/Documents/kart-4-3.7.db'
 
-const db = new sqlite3.Database('/Users/joy/Documents/kart-4-3.7.db', (err) => {
+const db = new sqlite3.Database(DB_PATH, (err) => {
   if (err) {
     return console.error(err.message)
   }
@@ -12,7 +19,7 @@ const db = new sqlite3.Database('/Users/joy/Documents/kart-4-3.7.db', (err) => {
 
 // 函数：发送HTTP请求，并返回Promise
 function sendHttpRequest(data) {
-  return axios.post('https://racing-7gxq1capbac7539a-1300165852.ap-shanghai.app.tcloudbase.com/postfromlocal', data, {
+  return axios.post(`https://racing-7gxq1capbac7539a-1300165852.ap-shanghai.app.tcloudbase.com/${CLOUD_FUNCTION_HTTPPATH}`, data, {
     Headers: {
       'Content-type': 'application/json'
     }
@@ -42,20 +49,12 @@ function processScoreLogs() {
       console.error('Error selecting from score_log', selectErr);
       return;
     }
-
-    // 为所有行发送HTTP请求，处理为Promise数组
-    const promises = rows.map(row => sendHttpRequest(row));
-    
-    // 等待所有请求完成
-    const results = await Promise.all(promises);
-    
-    // 筛选出成功上传的记录ID
-    const idsToUpdate = results
-      .filter(result => result.success)
-      .map(result => result.id);
-
-    // 如果有成功的上传，则执行批量删除
-    if (idsToUpdate.length > 0) {
+    if (rows.length) {
+      // 为所有行发送HTTP请求，处理为Promise数组
+      await sendHttpRequest(rows);
+      console.log(`上传${rows.length}数据成功`)
+      // 筛选出成功上传的记录ID
+      const idsToUpdate = rows.map(result => result.id);
       db.run(`UPDATE score_log SET uploaded = 1 WHERE id IN (${idsToUpdate.map(() => '?').join(',')})`, idsToUpdate, function(deleteErr) {
         if (deleteErr) {
           console.error('Error update records', deleteErr);
--- nodeScript/postScoreTest.js
+++ nodeScript/postScoreTest.js
@@ -1,7 +1,15 @@
 const axios = require('axios')
+const moment = require('moment')
 const CLOUD_FUNCTION_HTTPPATH = 'postfromlocal'
 // const CLOUD_FUNCTION_HTTPPATH = 'postdata'
-// 函数：发送HTTP请求，并返回Promise
+
+/**
+ * 模拟本地node服务查询本地 score_log 表中未上传的数据提交到云函数
+ * 根据上行数据中 car_name 匹配到当天绑定微信的中间表
+ * 把微信用户信息和 car_name ，圈速数据写入 racing-data表中
+ * @param {*} data 
+ * @returns 
+ */
 function sendHttpRequest(data) {
     return axios.post(`https://racing-7gxq1capbac7539a-1300165852.ap-shanghai.app.tcloudbase.com/${CLOUD_FUNCTION_HTTPPATH}`, data, {
       Headers: {
@@ -26,17 +34,21 @@ function sendHttpRequest(data) {
       });
   }
 
-  sendHttpRequest([
-    {
-      "car_name":"26号",
-      "dateTime":1712136373670,
-      "lap_create_time": "2024-04-10 20:26:08",
-      "single_score": 13333,
-    },
-    {
-      "car_name":"26号",
-      "dateTime":1712136373670,
-      "lap_create_time": "2024-04-10 20:26:08",
-      "single_score": 15555,
-    },
-  ])
+  (async function() {
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    const res = await sendHttpRequest([
+      {
+        "car_name":"26号",
+        "dateTime":1712136373670,
+        "lap_create_time": currentTime,
+        "single_score": 99999,
+      },
+      {
+        "car_name":"28号",
+        "dateTime":1712136373670,
+        "lap_create_time": currentTime,
+        "single_score": 33333,
+      },
+    ]);
+    console.log('上报成功：',res)
+  })()
