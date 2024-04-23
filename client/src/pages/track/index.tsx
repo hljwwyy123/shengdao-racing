5668aaa46090322c87bc2b26e584a7d43dc9ccd2
Aiden <466517204@qq.com>
2024-04-23 22:23:15 +0800

    文章样式

--- client/src/pages/track/index.tsx
+++ client/src/pages/track/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Taro, { useShareAppMessage } from '@tarojs/taro'
 import { RichText } from "@tarojs/components";
+import "./article.less"
 
 const REQUEST_URL = 'https://racing-7gxq1capbac7539a-1300165852.ap-shanghai.service.tcloudbase.com/article/v1.0/trace_desc'
+// const REQUEST_URL = 'https://racing-7gxq1capbac7539a.ap-shanghai.kits.tcloudbasegateway.com/cms/3c6z4xxteheglido/v1/open-api/projects/shengdao/collections/atricle/contents?limit=10&offset=0'
 
 export default function TrackIntro() {
   const [atricleDomString, setAtricle] = useState<any>('');
@@ -24,7 +26,7 @@ export default function TrackIntro() {
     const { data } = await Taro.request({
       url: REQUEST_URL,
       method: 'GET'
-    })
+    });
     setAtricle(data.data[0].desc)
   }
 
