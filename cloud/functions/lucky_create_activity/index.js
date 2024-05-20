6d6e24f7ebb290f31e884c9833d32f6444599ad1
Aiden <466517204@qq.com>
2024-05-20 23:12:37 +0800

    update

---
+++ cloud/functions/lucky_create_activity/index.js
@@ -0,0 +1,30 @@
+const tcb = require('@cloudbase/node-sdk')
+const cloud = require('wx-server-sdk')
+const moment = require('moment');
+
+cloud.init({
+  env: 'racing-7gxq1capbac7539a'
+})
+
+const tcbapp = tcb.init({
+  env: 'racing-7gxq1capbac7539a',
+  region: "ap-shanghai",
+});
+
+const db = tcbapp.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const { id } = event;
+  const payload = {
+    ...event, 
+    unionId: wxContext.FROM_UNIONID, 
+    createTime: moment(new Date().getTime()).format("YYYY-MM-DD HH:mm:ss"),
+    awardConfig: [],
+  };
+  if (id) {
+    return db.collection('lucky_activity_list').where({_id: id}).update(payload)
+  } else {
+    return  db.collection('lucky_activity_list').add(payload)
+  }
+}
